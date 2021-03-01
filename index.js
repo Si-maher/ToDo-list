eb2e951afefcb7edf266a32745eaa3dbf1762544
Simon Maher <simonmaher8307@hotmail.com>
2021-03-01 21:38:19 +0200

    created todo function, added variables

---
+++ index.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todoUL = document.getElementById('todo')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
+})
+
+function addTodo(todo) {
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
+    if(todoText) {
+        const todoElement = document.createElement('li')
+        if(todo && todo.completed) {
+            todoElement.classList.add('completed')
+        }
+    }
+}
